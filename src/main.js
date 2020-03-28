d69f1c864df3db2ef5298278fc463ecc2289c389
yzh <beiding110@gmail.com>
2020-03-28 12:00:52 +0800

    【新增】集成sentry功能，默认开启; 【新增】集成element-ui，默认开启；

--- src/main.js
+++ src/main.js
@@ -5,7 +5,29 @@ import App from './App'
 import router from './router'
 import store from '@/store'
 
+import Raven from 'raven-js';
+import RavenVue from 'raven-js/plugins/vue';
+
+if(config.element) {
+    const ElementUI = require('element-ui');
+    require('element-ui/lib/theme-chalk/index.css');
+    Vue.use(ElementUI);
+};
+
+import config from '../config'
+
 Vue.config.productionTip = false
+
+
+if(process.env.NODE_ENV !== 'development' ){
+    if(config.build.sentry) {
+        Raven.config(config.build.sentry.dsn).addPlugin(RavenVue, Vue).install();
+        Vue.config.errorHandler = function(err, vm, info) {
+            Raven.captureException(err);
+            console.error(err);
+        };
+    };
+};
 
 /* eslint-disable no-new */
 new Vue({
