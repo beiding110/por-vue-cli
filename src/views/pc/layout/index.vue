8f0290fc9edd32f7ef28d805efaf36f1f57441a7
yzh <beiding110@gmail.com>
2020-06-03 18:29:00 +0800

    【修改】layout新增导航折叠式；

--- src/views/pc/layout/index.vue
+++ src/views/pc/layout/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="layout">
-        <div class="left-nav" :class="{collapse:collapseController}">
+    <div class="layout" :class="{mobile:mobile, 'view-only':viewOnly}">
+        <div class="left-nav-cover" v-if="showController" @click="toggleShowController"></div>
+        <div class="left-nav" :class="{collapse:collapseController, show:showController}" v-if="!viewOnly">
             <el-menu
             router
             :collapse="collapseController"
@@ -8,18 +9,24 @@
             text-color="#BFCBD9"
             active-text-color="#ffd04b"
             default-active="2">
-                <my-nav-menu :list="menuList"></my-nav-menu>
-            </el-menu>
+            <my-nav-menu :list="menuList"></my-nav-menu>
+        </el-menu>
+    </div>
+    <div class="top-nav" :class="{collapse:collapseController}" v-if="!viewOnly">
+        <div class="btn-coll btn_top-nav" @click="toggleCollapse" v-if="!mobile">
+            <i :class="{'el-icon-s-fold':!collapseController,'el-icon-s-unfold':collapseController}"></i>
         </div>
-        <div class="top-nav" :class="{collapse:collapseController}">
-            <div class="btn-coll" @click="toggleCollapse">
-                <i :class="{'el-icon-s-fold':!collapseController,'el-icon-s-unfold':collapseController}"></i>
-            </div>
+        <div class="btn-coll btn_top-nav" @click="toggleCollapse" v-else>
+            <i class="el-icon-s-unfold"></i>
         </div>
-        <div class="view" :class="{collapse:collapseController}" id="view-content">
-            <router-view/>
+        <div class="btn-back btn_top-nav circle" @click="goBack">
+            <i class="el-icon-back"></i>
         </div>
     </div>
+    <div class="view" :class="{collapse:collapseController}" id="view-content">
+        <router-view/>
+    </div>
+</div>
 </template>
 
 <script>
@@ -27,7 +34,10 @@ export default {
     data () {
         return {
             collapseController: false,
-            menuList: []
+            showController: false,
+            menuList: [],
+            mobile: false,
+            viewOnly: false
         }
     },
     methods: {
@@ -37,11 +47,33 @@ export default {
             })
         },
         toggleCollapse() {
+            if(this.mobile) {
+                this.toggleShowController();
+                return;
+            };
             this.collapseController = !this.collapseController;
+        },
+        toggleShowController() {
+            this.showController = !this.showController;
+        },
+        goBack() {
+            this.$router.go(-1);
+        },
+        resizeHandler() {
+            if(window.innerWidth < 1000) {
+                this.mobile = true;
+            } else {
+                this.mobile = false;
+            };
         }
     },
     mounted: function() {
+        window.addEventListener('resize', this.resizeHandler);
+
         // this.queryMenu();
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.resizeHandler);
     }
 }
 </script>
@@ -61,13 +93,16 @@ $colLeftWidth: 64px;
             span{ height: 0; width: 0; overflow: hidden; visibility: hidden; display: inline-block;}
             .el-submenu__icon-arrow{display:none;}
         }
+
+
     }
-    .top-nav{height:50px; position:absolute; left:$leftWidth; top:0; right:0; box-shadow:0 1px 4px rgba(0,21,41,.08); transition:all .3s;
+    .top-nav{height:50px; position:absolute; left:$leftWidth; top:0; right:0; box-shadow:0 1px 4px rgba(0,21,41,.08); transition:all .3s; overflow:hidden;
         &.collapse{left:$colLeftWidth;}
 
-        .btn-coll{cursor:pointer; height:100%; display:inline-block; line-height:50px; padding:0 1em;
+        .btn_top-nav{cursor:pointer; height:100%; display:inline-block; line-height:50px; padding:0 1em; float:left; transition:.2s all;
             &:hover{background:#efefef;}
             i{font-size:22px;}
+            &.circle{border-radius:50%; width:2em; height:2em; line-height:2em; text-align:center; padding:0; margin-top:9px;}
         }
     }
     .view{position:absolute; left:$leftWidth; top:50px; right:0; bottom:0; padding:10px; overflow:auto; transition:all .3s;
@@ -75,5 +110,16 @@ $colLeftWidth: 64px;
     }
 }
 
+.layout.mobile{
+    .left-nav-cover{position:absolute; left:0; right:0; top:0; bottom:0; background:rgba(0,0,0,.3); z-index:998;}
+    .left-nav{transform:translateX(-$leftWidth); z-index:999;
+        &.show{transform:translateX(0);}
+    }
+    .top-nav{left:0;}
+    .view{left:0;}
+}
 
+.layout.view-only{
+    .view{left:0; top:0;}
+}
 </style>
