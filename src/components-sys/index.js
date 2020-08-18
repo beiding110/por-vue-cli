ee6e98fbf0f554ed930f2fc0e18cf9ac1114d1ec
yzh <beiding110@gmail.com>
2020-08-18 10:53:15 +0800

    【新增】info-tip组件； 【新增】my-btn组件； 【新增】my-tag组件； 【新增】btn-builder组件；

--- src/components-sys/index.js
+++ src/components-sys/index.js
@@ -1,5 +1,10 @@
 export default{
     install: function(Vue){
+        Vue.component('my-btn', function(){return import(/*webpackChunkName: 'coms-sys'*/ './my-btn')});
+        Vue.component('my-tag', function(){return import(/*webpackChunkName: 'coms-sys'*/ './my-tag')});
+        Vue.component('btn-builder', function(){return import(/*webpackChunkName: 'coms-sys'*/ './btn-builder')});
+
         Vue.component('zh-action-row', function(){return import(/*webpackChunkName: 'coms-sys'*/ './zh-action-row')});
+        Vue.component('info-tip', function(){return import(/*webpackChunkName: 'coms-sys'*/ './info-tip')});
     }
 }
