b0d2a5ec1ae5a3127f21efb6ca08ba2b16d35bbf
yzh <beiding110@gmail.com>
2021-03-18 16:25:42 +0800

    【新增】iframe页相关；

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import guards from './guards'
 
-import _ from './tools.js'
+import CONFIG from '../config'
+
+import { initRouter } from '@router/tools'
 
 Vue.use(Router);
 
-const autoMountRoutes = _.init();
+const indexs = initRouter();
 
 var router = new Router({
     routes: [
@@ -14,7 +16,41 @@ var router = new Router({
             path: '/demo/vcm',
             component: () => import(/*webpackChunkName: 'demo-vcm'*/ '@components/example')
         },
-        ...autoMountRoutes,
+        {
+            path: `/${CONFIG.router['base-name']}`,
+            component: () => import(/*webpackChunkName: 'pc'*/ '@layout/empty'),
+            meta: {
+                title: CONFIG.router['title']
+            },
+            children: indexs
+        }
+
+        // {
+        //     path: '/login',
+        //     component: () => import(/*webpackChunkName: 'pc-login'*/ '@views/login/index'),
+        //     meta: {
+        //         title: `${CONFIG.router['title']}·登录`
+        //     }
+        // },
+        // {
+        //     path: `/${CONFIG.router['base-name']}`,
+        //     component: () => import(/*webpackChunkName: 'pc'*/ '@views/layout/index'),
+        //     meta: {
+        //         title: CONFIG.router['title']
+        //     },
+        //     children: [
+        //         {
+        //             path: 'iframe',
+        //             component: () => import(/*webpackChunkName: 'iframe'*/ '@views/iframe'),
+        //             meta: {
+        //                 title: '',
+        //                 bread: []
+        //             }
+        //         },
+
+        //         ...indexs
+        //     ]
+        // }
     ]
 });
 
